81f6db64b69b10e94616045e1a99862d3e9717a0
BenoitSafari <gregory.michalak.192@gmail.com>
2023-12-10 01:36:46 +0100

    .gitignore

--- lib/types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as Button, type ButtonProps as ButtonProps } from './components/Button/Button';
-export { default as Loader, type LoaderProps as LoaderProps } from './components/Loader/Loader';
